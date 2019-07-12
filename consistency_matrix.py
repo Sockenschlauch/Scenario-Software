b4be4c8edb93fc3a1f38998f106dbd7a902d2096
Sockenschlauch <52604675+Sockenschlauch@users.noreply.github.com>
2019-07-12 16:20:38 +0200

    initial file write of consitency matrix

--- consistency_matrix.py
+++ consistency_matrix.py
@@ -1,4 +1,5 @@
 import openpyxl
+from openpyxl import Workbook
 
 path = r"E:\Git\Scenario-Software\SimpleTest_Factors.xlsx"
 
@@ -7,28 +8,41 @@ def read_projections(path):
     wb = openpyxl.load_workbook(path)
     sheet = wb.active
     cell_value = sheet
-    matrix = []
+    factor_list = []
 
     i = 0
     for row in sheet.iter_rows():
-        matrix.append([])
+        factor_list.append([])
         for cell in row:
             if cell.value:
-                matrix[i].append(cell.value)
+                factor_list[i].append(cell.value)
         i += 1
-    return matrix
+    return factor_list
 
 
-def print_projections(matrix):
-    for factor in matrix[1:]:
+def print_projections(factor_list):
+    for factor in factor_list[1:]:
         print(factor[0], end=": ")
         for projection in factor[1:-1]:
             print(projection, end=", ")
         print(factor[-1])
 
 
+def write_consistency_matrix(path, factor_list):
+    dst_filename = "consistency_matrix.xlsx"
+
+    wb = Workbook()
+    ws1 = wb.active
+    ws1.title = "consistency matrix"
+
+    wb.save(filename=dst_filename)
+
+
 def main():
-    print_projections(read_projections(path))
+    projections = read_projections(path)
+    print("Creating consistency matrix...")
+    write_consistency_matrix(path=path, factor_list=projections)
+    print("Consistency matrix created!")
 
 
 if __name__ == "__main__":

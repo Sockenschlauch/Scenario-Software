18e277d82812f633c53ace844fb963227af8b744
Sockenschlauch <52604675+Sockenschlauch@users.noreply.github.com>
2019-07-16 20:10:24 +0200

    added borders to the consistency matrix

--- consistency_matrix.py
+++ consistency_matrix.py
@@ -1,7 +1,12 @@
 import openpyxl
 from openpyxl import Workbook
+from openpyxl.styles.borders import Border, Side
 
 path = r"E:\Git\Scenario-Software\SimpleTest_Factors.xlsx"
+
+# defining the style of borders
+thin_border_bottom = Border(bottom=Side(style='thin'))
+thin_border_right = Border(right=Side(style='thin'))
 
 
 def read_projections(path):  # reads the list of factors with projections
@@ -36,28 +41,51 @@ def write_consistency_matrix(path, factor_list):
     ws1 = wb.active
     ws1.title = "consistency matrix"
 
+    len = get_projection_count(factor_list)
+
 # Writing the vertical projections
     row = 3
     colum = 1
     for factor in factor_list[1:]:
         for projection in factor[1:]:
-
             ws1.cell(row, colum).value = factor[0]
             ws1.cell(row, colum+1).value = projection
-
+            ws1.cell(row, colum+1).border = thin_border_right
             row += 1
+
+        for i in range(1, len+3):
+            if ws1.cell(row-1, i).border == Border(right=Side(style='thin')):
+                ws1.cell(row-1, i).border = Border(bottom=Side(style='thin'),
+                                                   right=Side(style='thin'))
+            else:
+                ws1.cell(row-1, i).border = thin_border_bottom
+
 # Writing the horizontal projections
     row = 1
     colum = 3
     for factor in factor_list[1:]:
         for projection in factor[1:]:
-
             ws1.cell(row, colum).value = factor[0]
             ws1.cell(row+1, colum).value = projection
-
+            ws1.cell(row+1, colum).border = thin_border_bottom
             colum += 1
 
+        for i in range(1, len+3):
+            if ws1.cell(i, colum-1).border == Border(bottom=Side(style='thin')):
+                ws1.cell(i, colum-1).border = Border(bottom=Side(style='thin'),
+                                                     right=Side(style='thin'))
+            else:
+                ws1.cell(i, colum-1).border = thin_border_right
+
     wb.save(filename=dst_filename)
+
+
+def get_projection_count(factor_list):  # returns the length of the factor list
+    factor_list_len = 0
+    for factor in factor_list[1:]:
+        for projection in factor[1:]:
+            factor_list_len += 1
+    return factor_list_len
 
 
 def main():

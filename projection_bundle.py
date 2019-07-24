cf8b1816dfd2e630bffe65d75d235b6ca64b6950
Sockenschlauch <52604675+Sockenschlauch@users.noreply.github.com>
2019-07-24 18:34:09 +0200

    added counter

--- projection_bundle.py
+++ projection_bundle.py
@@ -8,3 +8,27 @@ class projection_bundle():
     def set_consistency(matrix):
         self.consistency = self.consistency = calculate_consistency(
             matrix=matrix, bundle=bundle)
+
+
+class iterator():
+    _overflow = []
+    _counter = []
+
+    def __init__(self, projections):
+
+        for projection in projections:
+            _overflow.append(len(projection))
+            _counter.append(1)
+
+    def get_next():
+        _counter[0] += 1
+        for i in range(len(_counter)):
+            if _counter[i] > _overflow[i] and i == len(_counter):
+                return False
+            elif _counter[i] > _overflow[i]:
+                _counter[i] = 1
+                _counter[i+1] += 1
+        return _counter
+
+    def get_counter():
+        return _counter

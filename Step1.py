5a67ec52b1dda48877e85e4831035a826ef91f76
Sockenschlauch <52604675+Sockenschlauch@users.noreply.github.com>
2019-08-04 11:57:48 +0200

    ConsistencyMatrixCreation with GUI

--- Step1.py
+++ Step1.py
@@ -1,6 +1,10 @@
 from consistency_matrix_creation import *
+from easygui import *
 
 factors_path = r"E:\Git\Scenario-Software\SimpleTest_Factors.xlsx"
+
+factors_path = fileopenbox(msg='Choose the list of factors and projections!',
+                           title='Creating consistency matrix', default="*\SimpleTest_Factors.xlsx", filetypes=["*.xlsx"])
 
 projections = read_projections(factors_path)
 print("Creating consistency matrix...")
